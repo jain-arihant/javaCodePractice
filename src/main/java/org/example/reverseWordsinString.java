c201bd0f5656616a76468f0600d2ca8279a340e7
arihant.jain <arihantjain266@gmail.com>
2023-04-11 10:56:46 +0530

    Reversing the words in a string

---
+++ src/main/java/org/example/reverseWordsinString.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class reverseWordsinString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String input = sc.nextLine();
+        reverseString(input);
+    }
+    private static String reverseString(String input){
+        String output="";
+        String str[] = input.split(" ");
+        for (int i = str.length - 1; i>=0; i--){
+            output += str[i] + " ";
+        }
+        System.out.println(output);
+        output = output.substring(0,output.length() -1);
+        return output;
+    }
+}
